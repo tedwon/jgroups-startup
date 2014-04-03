c1de0079396ae3d12aab4eeb83404867f486c7d0
tedwon <iamtedwon@gmail.com>
2014-04-03 09:40:36 +0900

    Update jgroups version

--- src/main/script/run.McastSenderTest.sh
+++ src/main/script/run.McastSenderTest.sh
@@ -5,4 +5,4 @@
 ##                                                                          ##
 ### ====================================================================== ###
 
-java  -Djava.net.preferIPv4Stack=true -Xms32m -Xmx32m -Dfile.encoding=UTF-8 -classpath ../conf:../lib/jgroups-2.12.1.3.Final.jar:../lib/jgroups-startup-1.0.0-SNAPSHOT.jar:../lib/log4j-1.2.16.jar: org.jgroups.tests.McastSenderTest -mcast_addr 231.12.21.132 -port 45588 -ttl 250 -send_on_all_interfaces
+java  -Djava.net.preferIPv4Stack=true -Xms32m -Xmx32m -Dfile.encoding=UTF-8 -classpath ../conf:../lib/groups-3.4.3.Final.jar:../lib/jgroups-startup-1.0.0-SNAPSHOT.jar:../lib/log4j-1.2.16.jar: org.jgroups.tests.McastSenderTest -mcast_addr 231.12.21.132 -port 45588 -ttl 250 -send_on_all_interfaces
